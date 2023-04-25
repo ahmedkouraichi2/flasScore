4677eddec53d41a875f3580bdcbf57e47dcae98e
kourichiahmed2 <kouraichiahmed@gmailcom>
2023-04-25 20:51:48 +0200

    mise en place de theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ResultComponent } from './components/result/result.component';
+import { NewsComponent } from './components/news/news.component';
+import { ClassificationComponent } from './components/classification/classification.component';
+import { HeaderComponent } from './components/header/header.component';
+import { WordCapComponent } from './components/word-cap/word-cap.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    ResultComponent,
+    NewsComponent,
+    ClassificationComponent,
+    HeaderComponent,
+    WordCapComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/word-cap/word-cap.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WordCapComponent } from './word-cap.component';
+
+describe('WordCapComponent', () => {
+  let component: WordCapComponent;
+  let fixture: ComponentFixture<WordCapComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WordCapComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WordCapComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
